43ef90b91502a2fa6edcba147a1e38ce9b821909
apoplexi24 <shivanandanasharma@gmail.com>
2021-06-29 16:55:41 +0530

    Function declaration, jotted down a few functions I could use in the program on Word

---
+++ miniproject.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h> // used for delay(), getch() etc
+#include<windows.h> //gotoxy()
+#include<string.h> //use for strcomp(), strcopy(), strlen() etc
+#include<ctype.h> //use for toupper(), tolower(), etc
+#include<stdlib.h>
+
+char ans = 0;
+int ok;
+int b, valid = 0;
+
+// functions being declared
+void WelcomeScreen(void);
+void Title(void);
+void MainMenu(void);
+void LoginScreen(void);
+
