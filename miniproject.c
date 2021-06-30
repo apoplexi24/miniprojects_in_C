b3ccdc2f82e27439c5e2bdfe71c44e46ab704fb1
apoplexi24 <shivanandanasharma@gmail.com>
2021-06-30 20:46:18 +0530

    Added the file to the database. Loop implemented for adding more records if needed. PS - Finished the Add record block.

--- miniproject.c
+++ miniproject.c
@@ -78,7 +78,7 @@ void MainMenu(void)
 	printf("\n\t\t\t\t5. Delete Patients Record\n");
 	printf("\n\t\t\t\t6. Exit\n");
 	printf("\n\n\n\n\t\t\t\t Choose from 1 to 6: ");
-	scanf("%d", &choose);
+	scanf("%i", &choose);
 	
 	switch(choose)
 	{
@@ -222,4 +222,176 @@ void Add_rec(void)
 			goto B;
 		}
 	}
+	
+	// Sex of Patient entry
+	do
+	{
+		printf("\n\t\t\t Gender[F/M]: ");
+		scanf(" %c", &p.Gender);
+		if(toupper(p.Gender) == 'M' || toupper(p.Gender) = 'F')
+		{
+			ok = 1;
+		}
+		else
+		{
+			ok = 0;
+		}
+		if(!ok)
+		{
+			printf("\n\t\t Gender field contains Invalid Character. Please enter either F or M.");	
+		}
+	}
+	while(!ok);
+	
+	//Age entry
+	printf("\n\t\t\t Age: ");
+	scanf(" %i", &p.age);
+	
+	//Address entry
+	do
+	{
+		C:
+		printf("\n\t\t\t Address: ");
+		scanf("%s", p.Address);
+		p.Address[0]=toupper(p.Address[0]);
+		if(strlen(p.Address) > 20 || strlen(p.Address) < 4)
+		{
+			printf("\n\t Invalid \t The range of address is 4 chars to 20 chars. Please enter address again.");
+			goto C;
+		}
+	} 
+	while(!valid);
+	
+	// Contact number
+	do
+	{
+		D:
+		printf("\n\t\t\t Contact no: ");
+		scanf("%s",p.Contact_no);
+		if(strlen(p.Contact_no) > 10 || strlen(p.Contact_no)!=10)
+		{
+			printf("\n\t Invalid Entry. Contact number must contain 10 numbers. Please enter number again.");
+			goto D;
+		}
+		else
+		{
+			for(b = 0; b < strlen(p.Contact_no);b++)
+			{
+				if(!isalpha(p.Contact_no[b])
+				{
+					valid = 1;
+				}
+				else
+				{
+					valid = 0;
+					break;
+				}
+			}
+			if(!valid)
+			{
+				printf("\n\t\t Contact number contains invalid characters. Please enter number again.");
+				
+			}
+		}
+	}
+	while(!valid);
+	
+	// email entry
+	do
+	{
+		printf("\n\t\t\t Email: ");
+		scanf("%s", p.Email);
+		if(strlen(p.Email) > 30 || strlen(p.Email) < 8)
+		{
+			printf("\n\t Invalid Entry. Range of email is 8 chars to 30 chars. Please enter email again. ");
+		}
+	}
+	while(strlen(p.Email) > 30 || strlen(p.Email) < 8);
+	
+	// Problem entry
+	
+	E:
+	printf("\n\t\t\t Problem: ");
+	scanf("%s", p.Problem);
+	p.Problem[0] = toupper(p.Problem[0]);
+	if(strlen(p.Problem) > 15 || strlen(p.Problem) < 3)
+	{
+		printf("\n\t Invalid Entry. Range of problem is from 3 chars to 15 chars.");
+		goto E;
+	}
+	else 
+	{
+		for(b = 0; b < strlen(p.Problem); b++)
+		{
+			if (isalpha(p.Problem[b]))
+			{
+				valid = 1;
+			}
+			else 
+			{
+				valid = 0;
+				break;
+			}
+		}
+		if(!valid)
+		{
+			printf("\n\t\t Problem contains invalid characters. Please enter problem again. ");
+			goto E;
+		}
+	}
+	// Prescribed Doctor
+	F:
+	printf("\n\t\t\t Prescribed Doctor: ");
+	scanf("%s", p.Doctor);
+	p.Doctor[0]=toupper(p.Doctor[0]);
+	if (strlen(p.Doctor) > 30 || strlen(p.Doctor) < 3)
+	{
+		printf("\n\t Invalid Entry. Range of Doctor name is 3 chars to 30 chars.");
+		goto F;
+	}
+	else
+	{
+		for(b = 0; b < strlen(p.Doctor); b++)
+		{
+			if(isalpha(p.Doctor[b]))
+			{
+				valid = 1;
+			}
+			else
+			{
+				valid = 0;
+				break;
+			}
+		}
+		if(!valid)
+		{
+			printf("\n\t\t Doctor Name contains invalid characters. Please enter Doctor name again.");
+			goto F;
+		}
+	}
+	
+	fprintf(ek, " %s %s %c %i %s %s %s %s %s \n", p.First_Name, p.Last_Name, p.Gender, p.Address, p.Contact_no, p.Email, p.Doctor);
+	printf("\n\n\t\t\t Information Recorded Successfully");
+	fclose(ek); // ek file is closed
+	
+	sd:
+	getch();
+	printf("\n\n\t\t\t Do you want more records [Y/N]? ");
+	scanf(" %c", &ans);
+	if(toupper(ans) == 'Y')
+	{
+		Add_rec();
+	}
+	else if(toupper(ans)=='N')
+	{
+		printf("\n\t\t Thank You. ");
+		getch();
+		MainMenu();
+	}
+	else
+	{
+		printf("\n\t\t Invalid Input \n");
+		goto sd;
+	}
 }
+
